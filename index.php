9c3d05694eb4578385ad9e654c04a7bb730f2b87
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2021-03-06 00:55:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 <!-- single product layout: -->
 
-<p style="color:red;"> NOTICE: If you are paying with a credit card, please enter your information manually, to avoid errors. We are currently working to resolve this problem. Thank you for understanding. </p>
 <?php while(have_posts()): the_post();?><article class = "site-card"> 
 
 
