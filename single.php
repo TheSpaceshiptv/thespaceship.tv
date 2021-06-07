7641dcb584d03d12444d765936979ceda807f788
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2021-06-06 23:20:03 -0500

    Update single.php

--- single.php
+++ single.php
@@ -74,7 +74,7 @@ while ( have_posts() ): the_post();
 		    <p><i>Enjoying the show? Consider <?php echo do_shortcode( '[xoo_el_action type="register" display="link" text="SIGNING UP" redirect_to="same"]' ); ?> to catch our latest live shows!</i></p>
 		    <?php endif; ?>
 		    <p>...start transmission</p>
-	    	<?php echo convert($notes); ?>
+	    	<?php echo $notes; ?>
 		    <p>end transmission...</p>
 	    </div>
 	    <?php endif; ?>
