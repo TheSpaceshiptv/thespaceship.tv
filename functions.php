9588198e0a0ea8779b3cb861f04677aa70930621
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2021-06-08 10:01:47 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -118,7 +118,7 @@ function show_info() {
 
 function call_to_action(){
     echo '<p><i>Enjoying the show? Consider ';
-    if (!$loggedin) {
+    if ($loggedin) {
     echo do_shortcode( '[xoo_el_action type="register" display="link" text="SIGNING UP" redirect_to="same"]' );
     echo 'to catch our latest live shows!</i></p>';
     } else {
