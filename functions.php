eafc95dcf700a9e650f80ef0d3d36ef99995a440
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2021-06-08 03:09:35 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -114,7 +114,7 @@ function show_info() {
 
 
 function convert($input) {
-    $pattern = '(http(s)?://)?(([a-zA-Z0-9])([-\w]+\.)+([^\s\.]+[^\s]*)+[^,.\s])+(?=<)';
+    $pattern = '(http(s)?:\/\/)?(([a-zA-Z0-9])([-\w]+\.)+([^\s\.]+[^\s]*)+[^,.\s])+(?=<)';
     return $output = preg_replace($pattern, '<a href="http$2://$3">$0</a>', $input);
 }
 
