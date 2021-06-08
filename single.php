60e0a123c128e67cafa9c772512c27f06bcb26d2
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2021-06-08 03:36:55 -0500

    2

--- single.php
+++ single.php
@@ -75,7 +75,7 @@ while ( have_posts() ): the_post();
 		    <?php endif; ?>
 			<p><a class="donate button" href="<?php echo GET('siteurl'); ?>/checkout/?add-to-cart=1954">Click Here</a> to donate</p>
 		    <p>...start transmission</p>
-	    	<?php echo makeClickableLinks($notes); ?>
+	    	<?php echo make_links_clickable($notes); ?>
 		    <p>end transmission...</p>
 	    </div>
 	    <?php endif; ?>
