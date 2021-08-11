11963b88b95e18894b245ea0a8024159000c9e6a
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2021-08-11 11:33:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php get_header(); ?>
-<!-- single product layout: -->
 
 <?php while(have_posts()): the_post();?><article class = "site-card"> 
 
-
-
     <?php the_content(); ?>
-
 
 </article><?php endwhile;?>
 
