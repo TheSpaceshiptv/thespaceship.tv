aa569456694c6da921ddb34dc8e741857925d4cb
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2021-11-22 08:18:20 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -194,14 +194,14 @@ if($total_pages > 1) {
       echo ($x) . '">' . $x . '</a>';  
     }
   }
-  echo '<a href="#page-links">Cancel</a></div>';
+  echo '</div>';
 }
 if ($my_page == 2 && !$is_cat):
   echo '<a class="bubble" href="' . GET('siteurl') . '">⇦</a>';
 else:
   echo ($first_page) ? '' : '<a class="bubble" href="' . $prev_page . '">⇦</a>';
 endif;
-echo ($total_pages > 1) ? '<span class="page-display">Page <a class="current-page" href="#page-select">' . $my_page . '</a><a href="#page-links" class="current-page cancel">×</a> of ' . $total_pages . '</span>' : '';
+echo ($total_pages > 1) ? '<span class="page-display">Page <a class="current-page" href="#page-select">' . $my_page . '</a><a href="#page-links" class="current-page cancel">ₓ</a> of ' . $total_pages . '</span>' : '';
 
 echo ($last_page) ? '' : '<a class="bubble" href="' . $next_page . '">⇨</a>';
 ?>
