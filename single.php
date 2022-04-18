19cd267003837dcc17ea08dd40efcd91bf3762ab
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2022-04-17 19:15:00 -0500

    Update single.php

--- single.php
+++ single.php
@@ -80,6 +80,7 @@ while ( have_posts() ): the_post();
 		    }
 			
 			if ($loggedin == false) { call_to_action(); }
+			else { echo do_shortcode( '[wdpgk_donation_btn]' ); }
 			?>
 			
 		    <p>...start transmission</p>
