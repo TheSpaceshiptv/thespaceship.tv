c224f2f41cf8247335d2af7c0510812b000df7df
James Pederson <james@jpederson.com>
2022-04-21 18:32:08 -0500

    Remove the OpenGraph tags from the header so there's no conflict with the Open Graphite plugin.

--- header.php
+++ header.php
@@ -7,18 +7,12 @@
     <meta name="description" content="<?php echo bloginfo('description');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="<?php GET('sitename'); ?>" />
-    <meta property="og:description" content="<?php get_bloginfo('description');?>" />
-    <meta property="og:url" content="<?php GET('siteurl');?>" />
-    <meta property="og:image" content="<?php GET('themeurl')?>/assets/bg.jpg" />
-
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="<?php echo GET('googlefont')?>" rel="stylesheet">
     <link rel="icon" type="image/png" href="<?php GET('themeurl')?>/assets/logo.png">
     <link href="<?php bloginfo('template_url') ?>/style.css?ver=<?php echo rand(111,999) ?>" rel="stylesheet">
+    
     <?php wp_head(); ?>
-
 
 </head>
 <body <?php body_class();?>>
